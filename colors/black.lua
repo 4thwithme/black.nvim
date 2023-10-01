ab016ff27e2caed8db80f9daad0827ec74fc24db
4thwithme <aopopenko@gmail.com>
2023-10-01 17:59:21 +0200

    init v8

--- colors/black.lua
+++ colors/black.lua
@@ -1,9 +1,9 @@
--- package.loaded["lua.black"]                     = nil
--- package.loaded["lua.black.util"]                = nil
--- package.loaded["lua.black.colors"]              = nil
--- package.loaded["lua.black.colors.conditionals"] = nil
--- package.loaded["lua.black.functions"]           = nil
--- package.loaded["lua.black.highlights"]          = nil
--- package.loaded["lua.black.highlights.plugins"]  = nil
+package.loaded["lua.black"]                     = nil
+package.loaded["lua.black.util"]                = nil
+package.loaded["lua.black.colors"]              = nil
+package.loaded["lua.black.colors.conditionals"] = nil
+package.loaded["lua.black.functions"]           = nil
+package.loaded["lua.black.highlights"]          = nil
+package.loaded["lua.black.highlights.plugins"]  = nil
 
 require "black.util".load()
